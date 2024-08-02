175c56ea1df5a8469c479d41fcb70f1e53d6713a
Mohmmed.info <124050324+welecomtopython@users.noreply.github.com>
2024-08-02 05:14:32 +0300

    رفع مجلد كامل

---
+++ .vscode/t/t/event/help_option.py
@@ -0,0 +1,33 @@
+
+def init():
+    return ("""
+    init [Option ] 
+      yes -y      create .src config          
+    """)
+
+def set():
+    return ("""
+    set [Option]>
+    
+    -----------------
+    option
+        help , -h     print all help set
+        in   , -i     add or refresh prject
+        new  , -n     crieta new project     
+    """)
+
+def init():
+    return ("""
+    init [Option] > 
+    
+    ----------------
+    opthoin
+        
+        yes -y        creat file Docs.config.json confieg dir or prject
+        yes -y + .     input entry data prject insibd  file Docs.config.json 
+                                                confieg dir or prject
+
+        
+        
+        
+    """)
